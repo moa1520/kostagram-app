d1eacf926aec74debc5b822d353a841591af644f
moa1520 <douit_be@naver.com>
2019-12-27 20:04:01 +0900

    Messages Navigation Part II

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 import Home from "../screens/Tabs/Home";
 import Search from "../screens/Tabs/Search";
 import Notifications from "../screens/Tabs/Notifications";
 import Profile from "../screens/Tabs/Profile";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import MessagesLink from "../components/MessagesLink";
 
 const stackFactory = (initialRoute, customConfig) =>
   createStackNavigator({
@@ -20,11 +20,7 @@ export default createBottomTabNavigator({
   Home: {
     screen: stackFactory(Home, {
       title: "Home",
-      headerRight: (
-        <TouchableOpacity>
-          <Text>Hello</Text>
-        </TouchableOpacity>
-      )
+      headerRight: () => <MessagesLink />
     })
   },
   Search: {
