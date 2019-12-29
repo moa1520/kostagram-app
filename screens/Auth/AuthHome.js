c80e33382860099fd3a029e9bccdee35516d03af
moa1520 <douit_be@naver.com>
2019-12-29 21:23:22 +0900

    Auth Component

--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import constants from "../../constants";
+import AuthButton from "../../components/AuthButton";
 
 const View = styled.View`
   justify-content: center;
@@ -15,35 +16,20 @@ const Image = styled.Image`
 
 const Touchable = styled.TouchableOpacity``;
 
-const SignUpBtn = styled.View`
-  background-color: ${props => props.theme.blueColor};
-  padding: 10px;
-  margin: 0 50px;
-  border-radius: 4px;
-  width: ${constants.width / 2};
-  margin-bottom: 25px;
-`;
-
-const SignUpBtnText = styled.Text`
-  color: white;
-  text-align: center;
-  font-weight: 600;
-`;
-
 const LoginLink = styled.View``;
 
 const LoginLinkText = styled.Text`
   color: ${props => props.theme.blueColor};
+  margin-top: 20px;
 `;
 
 export default ({ navigation }) => (
   <View>
     <Image resizeMode={"contain"} source={require("../../assets/logo.png")} />
-    <Touchable onPress={() => navigation.navigate("Signup")}>
-      <SignUpBtn>
-        <SignUpBtnText>가입하기</SignUpBtnText>
-      </SignUpBtn>
-    </Touchable>
+    <AuthButton
+      text={"가입하기"}
+      onPress={() => navigation.navigate("Signup")}
+    />
     <Touchable onPress={() => navigation.navigate("Login")}>
       <LoginLink>
         <LoginLinkText>로그인</LoginLinkText>
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import AuthButton from "../../components/AuthButton";
+import AuthInput from "../../components/AuthInput";
+import useInput from "../../hooks/useInput";
 
 const View = styled.View`
   justify-content: center;
@@ -9,8 +12,17 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Text>Login</Text>
-  </View>
-);
+export default () => {
+  const emailInput = useInput("");
+  const handleLogin = () => {};
+  return (
+    <View>
+      <AuthInput
+        {...emailInput}
+        placeholder="이메일"
+        keyboardType="email-address"
+      />
+      <AuthButton onPress={() => null} text="로그인" />
+    </View>
+  );
+};
