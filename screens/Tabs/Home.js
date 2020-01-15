67d3bed8701de7aa0a9cee7b61ebf6ab672414ca
moa1520 <douit_be@naver.com>
2020-01-15 18:03:44 +0900

    Apollo Context

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import Loader from "../../components/Loader";
+
+const FEED_QUREY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -10,8 +42,7 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUREY);
+  return <View>{loading ? <Loader /> : <Text>Home</Text>}</View>;
+};
