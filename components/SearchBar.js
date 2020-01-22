46efda4b00a652d91c01467883e14dc02bf39dc9
moa1520 <douit_be@naver.com>
2020-01-22 18:09:40 +0900

    Search as HeaderTitle part II

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,9 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { TextInput } from "react-native";
+import constants from "../constants";
+import styles from "../styles";
 
 const SearchBar = ({ onChange, value, onSubmit }) => (
-  <TextInput value={value} placeholder={"검색"} />
+  <TextInput
+    style={{
+      width: constants.width - 40,
+      height: 35,
+      backgroundColor: styles.lightGreyColor,
+      padding: 10,
+      borderRadius: 5,
+      textAlign: "center"
+    }}
+    returnKeyType="search"
+    onChangeText={onChange}
+    onEndEditing={onSubmit}
+    value={value}
+    placeholder={"검색"}
+    placeholderTextColor={styles.darkGreyColor}
+  />
 );
 
 SearchBar.propTypes = {
