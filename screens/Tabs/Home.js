8bdf4e8e9b7518d53123cedfa0a4823c7d0bfdf7
moa1520 <douit_be@naver.com>
2020-01-22 15:43:12 +0900

    Post Component Header

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import Loader from "../../components/Loader";
+import Post from "../../components/Post";
 
 const FEED_QUREY = gql`
   {
@@ -41,8 +42,6 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
-
 export default () => {
   const [refreshing, setRefreshing] = useState(false);
   const { loading, data, refetch } = useQuery(FEED_QUREY);
@@ -63,7 +62,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Home</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };
