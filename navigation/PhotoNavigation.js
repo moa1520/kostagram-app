f55cf00f38ccf8bc0110b87f926d706114e7f8d7
moa1520 <douit_be@naver.com>
2020-01-31 03:58:49 +0900

    Select Photo part II

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -45,7 +45,7 @@ export default createStackNavigator(
     Tabs: {
       screen: PhotoTabs,
       navigationOptions: {
-        header: null
+        title: "사진 선택"
       }
     },
     UploadPhoto
