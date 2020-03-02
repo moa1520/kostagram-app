77d94d07090689a0d636be2ca66f1eba129dd94f
moa1520 <douit_be@naver.com>
2020-03-02 16:16:53 +0900

    version 1.1.1

--- screens/Tabs/Notifications.js
+++ screens/Tabs/Notifications.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useQuery } from "react-apollo-hooks";
 import SquarePhoto from "../../components/SquarePhoto";
-import { ScrollView } from "react-native";
+import { ScrollView, RefreshControl } from "react-native";
 import { EXPLORE_POST } from "../../components/Queries";
+import withSuspense from "../../components/withSuspense";
 
 const View = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
 `;
 
-const Text = styled.Text``;
-
 const Notifications = () => {
   const {
-    data: { explorePost }
+    data: { explorePost },
+    refetch
   } = useQuery(EXPLORE_POST, { suspend: true });
+  const [refreshing, setRefreshing] = useState(false);
+  const refresh = async () => {
+    try {
+      setRefreshing(true);
+      await refetch();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setRefreshing(false);
+    }
+  };
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={refresh} />
+      }
+    >
       <View>
         {explorePost.map(post => (
           <SquarePhoto key={post.id} {...post} />
